d437e96555ab38084b85893c52a64bdd5cce110b
Endri Dollani <endridollani99@gmail.com>
2024-03-31 10:35:53 +0200

    redux init

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,21 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
-import Page from './components/page/Page';
-import { BrowserRouter, Route, RouterProvider, Routes } from 'react-router-dom';
 import DashboardPage from './ui/dashboard/DashboardPage';
 import UnauthorizedPage from './ui/unauthorizedPage/UnauthorizedPage';
+import { store } from './state/store';
 
 
 const App: React.FC = () => (
-  <BrowserRouter>
-    <Routes>
-      <Route path='/dashboard' element={<DashboardPage/>} />
-      <Route path='*' element={<UnauthorizedPage/>} />
-    </Routes>
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/dashboard' element={<DashboardPage/>} />
+        <Route path='*' element={<UnauthorizedPage/>} />
+      </Routes>
+    </BrowserRouter>
+  </Provider>
 )
  
 export default App;
---
+++ src/state/reducers.ts
@@ -0,0 +1,6 @@
+import { combineReducers } from "@reduxjs/toolkit";
+
+const rootReducers = combineReducers({});
+
+
+export default rootReducers;
