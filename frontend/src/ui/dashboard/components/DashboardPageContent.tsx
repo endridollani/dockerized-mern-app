e3364335af83b68085a9be9c18c0534408b1eda3
Endri Dollani <endridollani99@gmail.com>
2024-04-07 21:46:00 +0200

    refactored ui of dashboard page

--- frontend/src/ui/dashboard/components/DashboardPageContent.tsx
+++ frontend/src/ui/dashboard/components/DashboardPageContent.tsx
@@ -1,12 +1,12 @@
-import React, { ReactNode } from 'react';
+import React, { useMemo } from 'react';
+import moment from 'moment';
+import { CancelOutlined, InfoOutlined } from '@mui/icons-material';
+import { Alert, Box, CircularProgress, IconButton, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@mui/material';
+
 import Page from '../../../components/page/Page';
 import { Car } from '../../types';
-import { Alert, Box } from '@mui/material';
-import BaseTypography from '../../../components/baseTypography/BaseTypography';
 import { CustomScroll } from 'react-custom-scroll';
-import { CancelOutlined } from '@mui/icons-material';
-import DashboardCellSkeleton from './DashboardCellSkeleton';
-import DashboardCell from './DashboardCell';
+import BaseTypography from '../../../components/baseTypography/BaseTypography';
 
 
 interface DashboardPageContentProps {
@@ -18,35 +18,96 @@ interface DashboardPageContentProps {
 const DashboardPageContent: React.FC<DashboardPageContentProps> = 
 ({ loading, errored, cars }) => {
 
-    let content: ReactNode = null;
+    const headerRows = cars?.length > 0 && (
+        <TableRow>
+            <TableCell>
+                <BaseTypography variant='body1' fontSize={'.8rem'} fontWeight={'bold'}>Name</BaseTypography>
+            </TableCell>
+            <TableCell>
+                <BaseTypography variant='body1' fontSize={'.8rem'} fontWeight={'bold'}>Acceleration</BaseTypography>
+            </TableCell>
+            <TableCell>
+                <BaseTypography variant='body1' fontSize={'.8rem'} fontWeight={'bold'}>Cylinders</BaseTypography>
+            </TableCell>
+            <TableCell>
+                <BaseTypography variant='body1' fontSize={'.8rem'} fontWeight={'bold'}>Displacement</BaseTypography>
+            </TableCell>
+            <TableCell>
+                <BaseTypography variant='body1' fontSize={'.8rem'} fontWeight={'bold'}>Horsepower</BaseTypography>
+            </TableCell>
+            <TableCell>
+                <BaseTypography variant='body1' fontSize={'.8rem'} fontWeight={'bold'} >Miles per Gallon</BaseTypography>
+            </TableCell>
+            <TableCell>
+                <BaseTypography variant='body1' fontSize={'.8rem'} fontWeight={'bold'}>Origin</BaseTypography>
+            </TableCell>
+            <TableCell>
+                <BaseTypography variant='body1' fontSize={'.8rem'} fontWeight={'bold'}>Weight in lbs</BaseTypography>
+            </TableCell>
+            <TableCell>
+                <BaseTypography variant='body1' fontSize={'.8rem'} fontWeight={'bold'}>Year</BaseTypography>
+            </TableCell>
+            <TableCell >
+                <BaseTypography variant='body1' fontSize={'.8rem'} fontWeight={'bold'}>Details</BaseTypography>
+            </TableCell>
+        </TableRow>
+    );
 
-    if (loading) {
-        content = <DashboardCellSkeleton />;
-    } else if (errored) {
-        content = <Alert icon={<CancelOutlined/>} color='error' variant='filled' sx={{ mt: '1rem'}}>Failed to fetch cars</Alert>;
-    } else {
-        if (cars.length) {
-            content = (
-                <Box sx={{ width: '100%', height: '80vh'}}>
-                    <CustomScroll heightRelativeToParent='100%'>
-                        <Box sx={{ pb: '4rem', display: 'flex', gap: '2rem', flexWrap: 'wrap'}}>
-                            {cars.map((car) => (<DashboardCell key={car._id} car={car} />))}
-                        </Box>
-                    </CustomScroll>
-                </Box> 
-            );
-        } else {
-           content = <BaseTypography variant='body2' fontStyle='italic'>There are no cars.</BaseTypography>
-        }
-    }
+    const bodyRows =useMemo(() => {
+        return (
+            <>
+                {cars?.length > 0 &&  cars.map((car) => (
+                    <TableRow key={car._id}>
+                        <TableCell>{car.Name}</TableCell>
+                        <TableCell>{car.Acceleration}</TableCell>
+                        <TableCell>{car.Cylinders}</TableCell>
+                        <TableCell>{car.Displacement}</TableCell>
+                        <TableCell>{car.Horsepower}</TableCell>
+                        <TableCell>{car.Miles_per_Gallon}</TableCell>
+                        <TableCell>{car.Origin}</TableCell>
+                        <TableCell>{car.Weight_in_lbs}</TableCell>
+                        <TableCell>{moment(car.Year).format('YYYY')}</TableCell>
+                        <TableCell>
+                            <IconButton>
+                                <InfoOutlined />
+                            </IconButton>
+                        </TableCell>
+                    </TableRow>
+                ))}
+            </>
+        );
+    }, [cars])
 
     return (
         <Page headerTitle='Dashboard'>
             <Box sx={{ pb: '.5rem', boxShadow: '0px 4px 4px -5px rgba(0,0,0,0.75)'}}>
-                <BaseTypography variant='subtitle2' sx={{ ml: '.4rem'}}>All</BaseTypography>
+                <BaseTypography variant='subtitle2' sx={{ ml: '.4rem'}}>Vehicles</BaseTypography>
             </Box>
             <Box sx={{ mt: '1rem' }}>
-                { content }
+                <Box sx={{ width: '100%', height: '80vh'}}>
+                    <CustomScroll heightRelativeToParent='100%'>
+                        <Box sx={{ pb: '4rem' }}>
+                           {loading && (
+                            <CircularProgress />
+                           )}
+                           {cars?.length > 0 && (
+                             <TableContainer component={Paper} >
+                                <Table>
+                                    <TableHead>
+                                        {headerRows}
+                                    </TableHead>
+                                    <TableBody>
+                                        {bodyRows}
+                                    </TableBody>
+                                </Table>
+                            </TableContainer>
+                           )}
+                           {errored && (
+                            <Alert variant='filled' color='error'  icon={<CancelOutlined />}>Something went wrong</Alert>
+                           )}
+                        </Box>
+                    </CustomScroll>
+                </Box>
             </Box>
         </Page>
     );
