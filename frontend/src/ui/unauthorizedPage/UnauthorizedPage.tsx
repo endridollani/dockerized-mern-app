9cc6eac81f64513c878571e293c6fe05af5c091f
Endri Dollani <endridollani99@gmail.com>
2024-04-10 23:54:30 +0200

    fixed unauthorized page

--- frontend/src/ui/unauthorizedPage/UnauthorizedPage.tsx
+++ frontend/src/ui/unauthorizedPage/UnauthorizedPage.tsx
@@ -1,24 +1,14 @@
-import React from 'react';
-import { Box, Button } from '@mui/material';
+import React, { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-
-import Page from '../components/page/Page';
-
 
 const UnauthorizedPage: React.FC = () => {
     const navigate = useNavigate();
 
-    const onClick = () => navigate('/dashboard')
+    useEffect(() => {
+        navigate('/dashboard')
+    }, [])
 
-    return (
-        <Page headerTitle='Unauthorized'>
-            <Box sx={{display: 'flex', justifyContent: 'center'}}>
-                <Button variant='contained' onClick={onClick}>
-                    Back to Dashboard
-                </Button>
-            </Box>
-        </Page>
-    );
+    return null;
 }
  
 export default UnauthorizedPage;
